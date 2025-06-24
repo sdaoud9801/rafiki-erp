6fa5e5fc6ea0d428bd45848d1b949df015660c48
sdaoud980 <sdaoud980@gmail.com>
2025-06-24 04:25:53 +0300

    reconfigure routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("./routes/purchase-form/purchase-form.tsx"),
-    route('purchases','./routes/purchase-form/purchase-form.tsx')
+    route('/purchases','./routes/purchases.tsx')
 ] satisfies RouteConfig;
---
+++ app/routes/purchases.tsx
@@ -0,0 +1,3 @@
+export default function purchases(){
+    return (<h1>Purchases</h1>)
+}
